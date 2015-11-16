05ab35cb99706dfa25e01ef4463368d810a89af1
FumiYama <f.yamanaka1127@gmail.com>
2015-11-17 01:39:53 +0900

    add animation button

--- practiceTouchIvent/ViewController.swift
+++ practiceTouchIvent/ViewController.swift
@@ -75,14 +75,19 @@ class ViewController: UIViewController {
     }
 
 
-
-
-
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for bTag in buttonTag.allValues {
+            let x = CGFloat(42*bTag.rawValue)
+            let btn = UIButton(frame: CGRectMake(x, self.view.bounds.height*9/10 , 40, 40))
+            btn.backgroundColor = UIColor.redColor()
+            btn.tag = bTag.rawValue
+            btn.setTitle(bTag.toStr(), forState: .Normal)
+            btn.addTarget(self, action: "pushedButton:", forControlEvents: .TouchUpInside)
+            self.view.addSubview(btn)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -91,8 +96,105 @@ class ViewController: UIViewController {
     }
     
     
+    enum buttonTag: Int {
+        case Zero = 1, One, Two, Three, Four, Five
+        static let allValues = [Zero, One, Two, Three, Four, Five]
+        
+        func toStr() -> String { // クラス内にStringに変換して取得できるメソッドを追加
+            return String(self.rawValue - 1)
+        }
+    }
     
-
+    func pushedButton(sender: UIButton) {
+        let btag = buttonTag(rawValue: sender.tag)!
+        
+//        if btag == .Other {
+//            print("Other button pushed!")
+//        } else {
+//            print("Pushed Number is \(btag.toStr())")
+//        }
+        
+        if btag == .Zero {
+            // バネのような動き
+            UIView.animateWithDuration(2.0, // アニメーションの時間を2秒に設定
+                delay: 0.0, // 遅延時間
+                usingSpringWithDamping: 0.1, // ばねの弾性力, 小さいほど弾性力が大きい
+                initialSpringVelocity: 1.5, // 初速度
+                options: UIViewAnimationOptions.CurveLinear, // 一定の速度
+                animations: { () -> Void in
+                    self.imageHere.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height*3/4)
+                    // アニメーション完了時の処理
+                }) { (Bool) -> Void in
+            }
+        } else if btag == .One { //X, Y方向にそれぞれ反転するアニメーション.
+            UIView.animateWithDuration(1.0,// アニメーションの時間を2秒に設定
+                animations: { () -> Void in // アニメーション中の処理
+                    self.imageHere.transform = CGAffineTransformScale(self.imageHere.transform, -1.0, 1.0) // X方向に反転用のアフィン行列作成
+                    // 連続したアニメーション処理.
+                }) { (Bool) -> Void in
+                    UIView.animateWithDuration(1.0,
+                        animations: { () -> Void in // アニメーション中の処理
+                            self.imageHere.transform = CGAffineTransformScale(self.imageHere.transform, 1.0, -1.0) // Y方向に反転用のアフィン行列作成
+                            // アニメーション完了時の処理
+                        }) { (Bool) -> Void in
+                    }
+                }
+        } else if btag == .Two { //回転アニメーション
+            let angle:CGFloat = CGFloat(M_PI_2) // radianで回転角度を指定
+        
+            UIView.animateWithDuration(2.0,
+                animations: { () -> Void in
+                self.imageHere.transform = CGAffineTransformMakeRotation(angle)// 回転用のアフィン行列を生成
+                }, completion: {(Bool) -> Void in
+            })
+            
+        } else if btag == .Three { // 拡縮アニメーション
+            UIView.animateWithDuration(2.0, animations: { () -> Void in
+                self.imageHere.transform = CGAffineTransformMakeScale(1.5, 1.5) // 拡大用アフィン
+            }) { (Bool) -> Void in
+                UIView.animateWithDuration(2.0, animations: { () -> Void in
+                    self.imageHere.transform = CGAffineTransformMakeScale(0.5, 0.5) //縮小用
+                }) { (Bool) -> Void in
+                    UIView.animateWithDuration(2.0, animations: { () -> Void in
+                        self.imageHere.transform = CGAffineTransformMakeScale(0.6, 0.6)
+                    })
+                }
+            }
+            
+        } else if btag == .Four { // 移動アニメーション
+//            imageHere.layer.position = CGPointMake(-30, -30)
+            
+            UIView.animateWithDuration(NSTimeInterval(CGFloat(1.0)), animations: { () -> Void in
+                self.imageHere.center = CGPoint(x: self.view.frame.width/4, y: self.view.frame.height/4)
+                }) { (Bool) -> Void in
+                    UIView.animateWithDuration(1.0, animations: { () -> Void in
+                    self.imageHere.center = CGPoint(x: self.view.frame.width*3/4, y: self.view.frame.height/4)
+                    })
+            }
+            
+        } else if btag == .Five { // 初期化
+            UIView.animateWithDuration(2.0, animations: { () -> Void in
+                self.imageHere.center = self.view.center //位置
+                self.imageHere.transform = CGAffineTransformMakeScale(1, 1) //大きさ
+                self.imageHere.transform = CGAffineTransformMakeRotation(0) // 回転
+            })
+            
+        }
+//
+//        switch btag {
+//            case Zero:
+//            
+//            case One:
+//                UIView.anima
+//            case Two:
+//            break
+//            case .Other:
+//            break
+//                        
+//        }
+    
+    }
+    
 
 }
 
